9109d9dcc1102e0a00e2900a540558310bef46fa
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2026-09-17 00:11:15 +0300

    Refactor main function and improve null handling

---
+++ task02.kt
@@ -0,0 +1,30 @@
+import kotlin.random.Random
+
+fun generateRandomArray(size: Int, maxValue: Int): IntArray? {
+    if (size <= 0 || maxValue <= 0) return null
+    
+    return IntArray(size) { Random.nextInt(0, maxValue + 1) }
+}
+
+fun main() {
+    generateRandomArray(size = 10, maxValue = 50)
+        ?.apply {
+            for (i in indices) {
+                this[i] = if (this[i] % 2 == 0) {
+                    this[i] / 2
+                } else {
+                    this[i] * 2
+                }
+            }
+        }
+        ?.also {
+            println("Модифікований масив: ${it.contentToString()}")
+        }
+        ?.run {
+            maxOrNull()
+        }
+        ?.let { 
+            println("Максимальне значення масиву: $it") 
+        } 
+        ?: println("Помилка вхідних даних")
+}
