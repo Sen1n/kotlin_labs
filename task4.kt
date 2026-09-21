b67ece975a258e137b997bb2ad3c0b5e83661968
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2026-09-21 21:44:00 +0300

    Create task4.kt

---
+++ task4.kt
@@ -0,0 +1,30 @@
+interface Messenger {
+    fun sendMessage(message: String)
+    fun receiveMessage(): String
+}
+
+class BasicMessenger : Messenger {
+    override fun sendMessage(message: String) {
+        println("Sending message: $message")
+    }
+
+    override fun receiveMessage(): String {
+        return "You've got a new message!"
+    }
+}
+
+class SmartMessenger(private val basicMessenger: BasicMessenger) : Messenger by basicMessenger {
+    override fun sendMessage(message: String) {
+        println("Sending a smart message: $message")
+        basicMessenger.sendMessage("[smart] $message")
+    }
+}
+
+fun main() {
+    val basicMessenger = BasicMessenger()
+    val smartMessenger = SmartMessenger(basicMessenger)
+    
+    basicMessenger.sendMessage("Hello!")
+    println(smartMessenger.receiveMessage())
+    smartMessenger.sendMessage("Hello from SmartMessenger!")
+}
